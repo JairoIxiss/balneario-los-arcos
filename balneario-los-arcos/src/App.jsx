242503f96d4fe271b53f0dd248c972d0e24cbee4
JairoIxiss <jairoixis7@gmail.com>
2026-07-20 16:40:39 -0600

    act

--- balneario-los-arcos/src/App.jsx
+++ balneario-los-arcos/src/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { HashRouter as Router, Routes, Route } from 'react-router-dom'
 import Navbar from './componentes/Navbar'
 import Main from './componentes/Main'
 import AguasTermales from './componentes/AguasTermales'
